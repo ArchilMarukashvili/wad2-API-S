3f9c796269bcf182ab1f10a7f7bd9548ec0be734
Archil Marukashvili <20106884@mail.wit.ie>
2025-12-14 19:47:37 +0000

    Tasks related to User - lab completed!

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -4,20 +4,27 @@ import asyncHandler from 'express-async-handler';
 
 const router = express.Router();
 
-router.get('/', asyncHandler(async (req, res) => {
-    const tasks = await Task.find().populate('userId', 'username');
+// Get a user's tasks
+router.get('/', async (req, res) => {
+    console.log(req.user);
+    const tasks = await Task.find({ userId: `${req.user._id}`});
     res.status(200).json(tasks);
-}));
+});
+
 
 router.get('/user/:id', asyncHandler(async (req, res) => {
     const tasks = await Task.find({ userId: req.params.id });
     res.status(200).json(tasks);
 }));
 
+// create a task
 router.post('/', asyncHandler(async (req, res) => {
-    const task = await Task(req.body).save();
+    const newTask = req.body;
+    newTask.userId = req.user._id;
+    const task = await Task(newTask).save();
     res.status(201).json(task);
 }));
+
 
 router.put('/:id', asyncHandler(async (req, res) => {
     if (req.body._id) delete req.body._id;
@@ -34,14 +41,17 @@ router.put('/:id', asyncHandler(async (req, res) => {
     }
 }));
 
-router.delete('/:id', asyncHandler(async (req, res) => {
-    const result = await Task.deleteOne({ _id: req.params.id });
-
+// delete Task
+router.delete('/:id', async (req, res) => {
+    const result = await Task.deleteOne({
+        _id: req.params.id,
+    });
     if (result.deletedCount) {
         res.status(204).json();
     } else {
         res.status(404).json({ code: 404, msg: 'Unable to find Task' });
     }
-}));
+});
+
 
 export default router;
